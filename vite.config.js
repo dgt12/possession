17280da1efce2795691ea9cecdbb3bf132357399
David Trye <davidtrye@gmail.com>
2024-02-03 14:23:14 +1300

    Corrected file path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/multicat/",
+  base: "/possession/",
   plugins: [svelte()],
   resolve: {
     alias: {
